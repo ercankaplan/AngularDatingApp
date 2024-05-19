01a6e88f2759e484c7a5079996efb003073981a9
ercan kaplan <ercan.kaplan@setur.com.tr>
2024-05-19 15:27:08 +0300

    API project of My First Angular Web App

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+    private readonly DataContext _context = context;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() //api/users
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id) //api/users/3
+    {
+        return await _context.Users.FindAsync(id);
+    }
+}
+
+
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+/// <summary>
+/// Represents the database context for the application.
+/// </summary>
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
+
+
+

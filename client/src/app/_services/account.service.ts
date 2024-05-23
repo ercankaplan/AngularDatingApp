e5b3e64018e0278856bf5c592a7a3173544cef3b
ercan kaplan <ercan.kaplan@setur.com.tr>
2024-05-23 22:00:11 +0300

    added login register pages

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { User } from '../_model/user';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'//This tells Angular to provide this service in the root module (AppModule)
+})
+
+// This service will be used to make HTTP requests to the API
+//Singletons are services that are shared across the application.
+//This means that the service is created once and shared across the application.
+export class AccountService {
+
+  baseUrl = 'https://localhost:5001/api/';
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http:HttpClient) {}
+
+
+  login(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response:User) => {
+        const user = response;
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        return user;
+      })
+    );
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+
+  setCurrentUser(user: User){
+    this.currentUserSource.next(user);
+  }
+
+  register(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map((user:User) => {
+        if(user){
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        return user;
+      })
+    );
+  }
+
+}

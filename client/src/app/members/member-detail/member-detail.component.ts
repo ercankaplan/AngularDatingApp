0c624e63a13570db74968e2d3770bd8be7376023
ercan kaplan <ercan.kaplan@setur.com.tr>
2024-06-03 23:01:06 +0300

    added ng gallery

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,16 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryModule, GalleryItem, ImageItem } from 'ng-gallery';
 import { Member } from 'src/app/_model/member';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
+  standalone: true,
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  imports: [CommonModule, TabsModule, GalleryModule]
 })
 export class MemberDetailComponent implements OnInit {
 
   member: Member | undefined;
+  images: GalleryItem[] = [];
 
   constructor(private memberService: MembersService, private route: ActivatedRoute) {
 
@@ -29,8 +35,22 @@ export class MemberDetailComponent implements OnInit {
 
   getMember(username: string) {
     this.memberService.getMember(username).subscribe({
-      next: (member) => this.member = member
+      next: (member) => {
+        this.member = member;
+        this.getPhotos();
+      }
     });
+  }
+
+  getPhotos() {
+
+    if (!this.member) return;
+    for (let photo of this.member?.photos) {
+      this.images.push(new ImageItem({ src: photo?.url, thumb: photo?.url, alt: photo?.url }));
+      this.images.push(new ImageItem({ src: photo?.url, thumb: photo?.url, alt: photo?.url }));
+      this.images.push(new ImageItem({ src: photo?.url, thumb: photo?.url, alt: photo?.url }));
+      this.images.push(new ImageItem({ src: photo?.url, thumb: photo?.url, alt: photo?.url }));
+    }
   }
 
 }

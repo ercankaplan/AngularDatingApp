a13954361ad50878ea999a7e434edfc505307c35
ercan kaplan <ercan.kaplan@setur.com.tr>
2024-06-08 17:03:20 +0300

    added spinner, storing data at service

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { Member } from '../_model/member';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,8 @@ import { Member } from '../_model/member';
 export class MembersService implements OnInit {
 
   baseUrl = environment.apiUrl;
+  
+  members: Member[] = [];
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -17,16 +20,40 @@ export class MembersService implements OnInit {
 
   getMembers() {
 
-    return this.http.get<Member[]>(this.baseUrl + 'members');//as of now, jwt.interceptor.ts is gonna do this
+    if(this.members.length > 0) 
+      return of(this.members);
+
+     return this.http.get<Member[]>(this.baseUrl + 'members').pipe(
+       map(members => {this.members = members; 
+        return members;})
+     )//as of now, jwt.interceptor.ts is gonna do this
+
+
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.userName === username);
+    console.log(this.members);
+    if(member) return of(member);
     return this.http.get<Member>(this.baseUrl + 'members/' + username);
   }
 
   updateMember(member: Member) {
+        return this.http.put(this.baseUrl + 'users', member).pipe(
+          map(()=> {
+            const index = this.members.indexOf(member);
+            //this.members[index] = member; This line of code replaces the object at this.members[index] with member
+            this.members[index] = {...this.members[index],...member};
+            /*
+            This line of code creates a new object that combines the 
+            properties of this.members[index] and member. 
+            If a property exists in both objects, the value from member will be used. 
+            This is known as object spread syntax. 
+            It's a way of creating a new object that includes properties from other objects.
+            */
+          })
 
-    return this.http.put(this.baseUrl + 'users', member)
+        )
   }
 
 
@@ -58,3 +85,4 @@ export class MembersService implements OnInit {
 
 
 }
+

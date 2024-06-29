15f50c1709fa577619fe33e8918ba27a1f96368e
ercan kaplan <ercan.kaplan@setur.com.tr>
2024-06-29 18:10:10 +0300

    enhanced register form

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,11 +4,29 @@ using System.ComponentModel.DataAnnotations;
 namespace API.DTOs;
 public class RegisterDto
 {
-  
-  [Required(ErrorMessage = "Username is required", AllowEmptyStrings = false)]
-    public string Username { get; set; }
 
-    [StringLength(8, MinimumLength = 4)]
-    [Required(ErrorMessage = "Password is required", AllowEmptyStrings = false)]
-    public string Password { get; set; }
+  [Required(ErrorMessage = "Username is required", AllowEmptyStrings = false)]
+  public string Username { get; set; }
+
+  [Required]
+  public string KnownAs { get; set; }
+
+  [Required]
+  public string Gender { get; set; }
+
+  [Required]
+  public DateOnly? DateOfBirth { get; set; }// optional to make it required work!
+
+
+  [Required]
+  public string City { get; set; }
+
+  [Required]
+  public string Country { get; set; }
+
+
+  [StringLength(8, MinimumLength = 4)]
+  [Required(ErrorMessage = "Password is required", AllowEmptyStrings = false)]
+  public string Password { get; set; }
+
 }
